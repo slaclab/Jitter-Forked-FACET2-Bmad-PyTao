ba038ffdaf8fc36b75c81583ce6bf49c8c784529
Nathan Majernik <nmajik@dhcp-visitor-218-219.slac.stanford.edu>
2024-10-17 14:08:19 -0700

    Ongoing work with new IMPACT two-bunch

--- UTILITY_quickstart.py
+++ UTILITY_quickstart.py
@@ -494,5 +494,32 @@ def centerBeam(
     return
 
 
+def calcBMAG(b0, a0, b, a):
+    #From Lucretia
+    #For a bit more detail, see "BMAG from Lucretia.nb"
+    #Not validated!!!
 
+    # function [B,Bpsi]=bmag(b0,a0,b,a)
+    # %
+    # % [B,Bpsi]=bmag(b0,a0,b,a);
+    # %
+    # % Compute BMAG and its phase from Twiss parameters
+    # %
+    # % INPUTs:
+    # %
+    # %   b0 = matched beta
+    # %   a0 = matched alpha
+    # %   b  = mismatched beta
+    # %   a  = mismatched alpha
+    # %
+    # % OUTPUTs:
+    # %
+    # %   B    = mismatch amplitude
+    # %   Bpsi = mismatch phase (deg)
+
+    g0 = (1 + a0 ** 2) / b0
+    g  = (1 + a ** 2) / b
+    B  = (b0 * g - 2.0 * a0 * a + g0 * b) / 2
+
+    return B
     
